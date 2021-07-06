3b0f58cb29149b1dd2aeb6d9df6122c388e9fb51
Pavel Kulagin <60746011+m1ntm1nd@users.noreply.github.com>
2021-07-06 22:49:46 +0300

    1

--- good/extensions/Mintable.sol
+++ good/extensions/Mintable.sol
@@ -2,11 +2,12 @@
 
 pragma solidity ^0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/AccessControl.sol";
 import "https://github.com/m1ntm1nd/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/m1ntm1nd/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
-contract KERC is ERC20, ERC20Burnable{
+contract KERC is ERC20, ERC20Burnable, AccessControl{
     constructor() ERC20("KERC20", "K20") public {
     }
 }
