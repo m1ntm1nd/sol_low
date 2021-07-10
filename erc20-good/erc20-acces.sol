c57af700a9fb151f277b27de21608f1c6d716add
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2021-07-10 12:23:08 +0300

    erc20-self 1.1

---
+++ erc20-good/erc20-acces.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/AccessControl.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+
+contract KERC is ERC20, ERC20Burnable, AccessControl {
+    
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    
+    constructor() ERC20("KERC20", "K20") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+    
+    function promoteMinter(address minter) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(MINTER_ROLE, minter);
+    }
+    
+    function demoteMinter(address minter) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(MINTER_ROLE, minter);
+    }
+    
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}
---
+++ extensions/Mintable.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/AccessControl.sol";
+import "https://github.com/m1ntm1nd/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/m1ntm1nd/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+
+contract KERC is ERC20, ERC20Burnable, AccessControl{
+    constructor() ERC20("KERC20", "K20") public {
+    }
+}
