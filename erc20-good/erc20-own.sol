50a7cd507aeb157dda2db31055fb02987306e094
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2021-07-10 12:42:24 +0300

    folders digest

--- erc20-good/erc20-own.sol
+++ erc20-good/erc20-own.sol
@@ -2,88 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import "../interface/IERC20.sol";
+import "../interface/IERC20Metadata.sol";
 import "../libs/SafeMath.sol";
-
-
-interface IERC20Metadata is IERC20 {
-
-
-    function name() external view returns (string memory);
-
-    function symbol() external view returns (string memory);
-
-    function decimals() external view returns (uint8);
-}
-
-
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
-
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    constructor() {
-        _setOwner(_msgSender());
-    }
-
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-    
-    function renounceOwnership() public virtual onlyOwner {
-        _setOwner(address(0));
-    }
-
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _setOwner(newOwner);
-    }
-
-    function _setOwner(address newOwner) private {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-}
-
-abstract contract Mintable is Context, Ownable {
-    
-    mapping(address => bool) private _minters;
-    
-    event MinterTrueFalse(address indexed minter, bool statement);
-    
-    function promoteMinter(address minter) public virtual onlyOwner{
-        _minters[minter] = true;
-        
-        emit MinterTrueFalse(minter, true);
-    }
-    
-    function demoteMinter(address minter) public virtual onlyOwner {
-        _minters[minter] = false;
-        
-        emit MinterTrueFalse(minter, false);
-    }
-    
-    modifier onlyMinter(){
-        require(_minters[_msgSender()] != false, "Mintable: caller is not the minter");
-        _;
-    }
-}
+import "../extensions/Mintable.sol";
 
 
 contract KERC20 is Context, IERC20, IERC20Metadata, Ownable, Mintable {  
@@ -128,7 +49,7 @@ contract KERC20 is Context, IERC20, IERC20Metadata, Ownable, Mintable {
         return _balances[account];
     }
 
-    function transfer(address recipient, uint amount) override public returns(bool) { 
+    function transfer(address recipient, uint amount) public override returns (bool) { 
 
         _balances[_msgSender()] = _balances[_msgSender()].sub(amount);
         _balances[recipient] = _balances[recipient].add(amount);
@@ -150,7 +71,7 @@ contract KERC20 is Context, IERC20, IERC20Metadata, Ownable, Mintable {
         address sender,
         address recipient,
         uint amount
-    ) public virtual override returns(bool){
+    ) public virtual override returns (bool) {
         
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
